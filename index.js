b810f6426f6a83d6d864f73ab077965a26b2dc39
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-13 10:00:35 +0530

    Create question field

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('CLI Calculator :');
+"use strict";
+exports.__esModule = true;
+console.log('CLI Calculator :\n');
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)('Enter First number: ');
+    var operator = (0, readline_sync_1.question)('Enter Operator: ');
+    var secondStr = (0, readline_sync_1.question)('Enter Second number: ');
+    console.log(firstStr, operator, secondStr);
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('CLI Calculator :');
+console.log('CLI Calculator :\n');
+
+import { question } from "readline-sync";
+
+function main(): void {
+    const firstStr: string = question('Enter First number: ');
+    const operator: string = question('Enter Operator: ');
+    const secondStr: string = question('Enter Second number: ');
+
+
+    console.log(firstStr, operator, secondStr);
+
+}
+
+main();
